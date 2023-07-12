a6deac17bf72ae41d6c0ca49ecc99e75f593805d
graziellabianco <grazybianco@gmail.com>
2023-07-12 08:15:18 -0300

    Criar componente de Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,30 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './modules/home/home.component';
 
+import{CardModule} from "primeng/card";
+import {InputTextModule}from "primeng/inputtext";
+import {ButtonModule} from "primeng/button";
+import {ToastModule } from "primeng/toast";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+
+    //Prime NG
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
